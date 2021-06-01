2ebcdce3da9e4e4438ff4184f6989262b101ef07
gustavo <gustavo.isaac@gmail.com>
2021-06-01 18:48:47 +0100

    Create MyApplication class and NetworkModule

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/DemeterRecipesApplication.kt
@@ -0,0 +1,7 @@
+package com.artemissoftware.demeterrecipes
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DemeterRecipesApplication: Application()
