8dca8dd33fa8848c9667d6f33b561568103a4235
gustavo <gustavo.isaac@gmail.com>
2021-06-07 18:42:21 +0100

    Create DAO (RecipesDao)

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/database/dao/RecipesDao.kt
@@ -0,0 +1,19 @@
+package com.artemissoftware.demeterrecipes.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.artemissoftware.demeterrecipes.database.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    fun readRecipes(): Flow<List<RecipesEntity>>
+
+}
