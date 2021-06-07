0f26331ac91ce4a3528fcc1898fe3eefa894f932
gustavo <gustavo.isaac@gmail.com>
2021-06-07 18:38:38 +0100

    Create Entity (RecipesEntity)

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/database/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.demeterrecipes.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.artemissoftware.demeterrecipes.api.models.FoodRecipe
+import com.artemissoftware.demeterrecipes.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+        var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
