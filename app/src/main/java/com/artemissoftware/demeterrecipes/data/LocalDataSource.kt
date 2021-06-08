85b51bcaaed2de1d2af8bd225ed44b320cc39f6b
gustavo <gustavo.isaac@gmail.com>
2021-06-08 12:50:10 +0100

    Create and Inject LocalDataSource

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/data/LocalDataSource.kt
@@ -0,0 +1,20 @@
+package com.artemissoftware.demeterrecipes.data
+
+import com.artemissoftware.demeterrecipes.database.dao.RecipesDao
+import com.artemissoftware.demeterrecipes.database.entities.RecipesEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class LocalDataSource @Inject constructor(
+        private val recipesDao: RecipesDao
+) {
+
+    fun readDatabase(): Flow<List<RecipesEntity>> {
+        return recipesDao.readRecipes()
+    }
+
+    suspend fun insertRecipes(recipesEntity: RecipesEntity) {
+        recipesDao.insertRecipes(recipesEntity)
+    }
+
+}
