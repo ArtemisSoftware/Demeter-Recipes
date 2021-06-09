5db14a57f3fde1447b35a42a31961f9605c84731
gustavo <gustavo.isaac@gmail.com>
2021-06-09 10:43:26 +0100

    Design RecipesBottomSheet

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/recipes/RecipesBottomSheetFragment.kt
@@ -0,0 +1,14 @@
+package com.artemissoftware.demeterrecipes.ui.fragments.recipes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.demeterrecipes.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+
+class RecipesBottomSheetFragment : BottomSheetDialogFragment() {
+
+}
