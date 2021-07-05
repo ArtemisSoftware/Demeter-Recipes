a8e4c837ff97f44587322ce6f32333d4b6d742d6
gustavo <gustavo.isaac@gmail.com>
2021-07-05 18:44:17 +0100

    Design activity_details Layout

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.artemissoftware.demeterrecipes.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.artemissoftware.demeterrecipes.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
