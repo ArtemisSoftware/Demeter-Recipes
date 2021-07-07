2692aa5cc2a1f6b98402b56ccb911fcda850f785
gustavo <gustavo.isaac@gmail.com>
2021-07-07 20:16:51 +0100

    Send Result to DetailsActivity - Safe Args

--- app/src/main/java/com/artemissoftware/demeterrecipes/bindingadapters/RecipesBinding.kt
+++ app/src/main/java/com/artemissoftware/demeterrecipes/bindingadapters/RecipesBinding.kt
@@ -1,16 +1,37 @@
 package com.artemissoftware.demeterrecipes.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import com.artemissoftware.demeterrecipes.api.models.FoodRecipe
+import com.artemissoftware.demeterrecipes.api.models.Result
 import com.artemissoftware.demeterrecipes.database.entities.RecipesEntity
+import com.artemissoftware.demeterrecipes.ui.fragments.recipes.RecipesFragmentDirections
 import com.artemissoftware.demeterrecipes.util.NetworkResult
 
 class RecipesBinding {
 
     companion object {
+
+
+        @BindingAdapter("onRecipeClickListener")
+        @JvmStatic
+        fun onRecipeClickListener(recipeRowLayout: ConstraintLayout, result: Result) {
+            Log.d("onRecipeClickListener", "CALLED")
+            recipeRowLayout.setOnClickListener {
+                try {
+                    val action = RecipesFragmentDirections.actionRecipesFragmentToDetailsActivity(result)
+
+                    recipeRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("onRecipeClickListener", e.toString())
+                }
+            }
+        }
 
 
         @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
