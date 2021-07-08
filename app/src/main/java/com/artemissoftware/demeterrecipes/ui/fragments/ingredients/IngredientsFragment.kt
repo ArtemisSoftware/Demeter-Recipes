e19ab5901ae5352e67365d147bbca19bf77a4600
gustavo <gustavo.isaac@gmail.com>
2021-07-08 18:57:11 +0100

    Create PagerAdapter class

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/ingredients/IngredientsFragment.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.demeterrecipes.ui.fragments.ingredients
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.demeterrecipes.R
+
+
+class IngredientsFragment : Fragment(R.layout.fragment_ingredients) {
+
+}
---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/instructions/InstructionsFragment.kt
@@ -0,0 +1,13 @@
+package com.artemissoftware.demeterrecipes.ui.fragments.instructions
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.demeterrecipes.R
+
+
+class InstructionsFragment : Fragment(R.layout.fragment_instructions) {
+
+}
