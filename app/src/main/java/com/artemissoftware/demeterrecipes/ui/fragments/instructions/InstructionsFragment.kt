5f358c39fca156ad6f4f272526949e1fb9111d23
gustavo <gustavo.isaac@gmail.com>
2021-07-12 10:15:27 +0100

    Modify InstructionsFragment

--- app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,9 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.artemissoftware.demeterrecipes.R
+import com.artemissoftware.demeterrecipes.api.models.Result
+import com.artemissoftware.demeterrecipes.util.Constants
+import kotlinx.android.synthetic.main.fragment_instructions.*
 
 
 class InstructionsFragment : Fragment(R.layout.fragment_instructions) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        instructions_webView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = myBundle!!.sourceUrl
+        instructions_webView.loadUrl(websiteUrl)
+    }
 
 }
