65cffaec634726c59776dc1c995768295134979b
gustavo <gustavo.isaac@gmail.com>
2021-07-12 14:50:26 +0100

    Modify RecipesDao interface

--- app/src/main/java/com/artemissoftware/demeterrecipes/database/dao/RecipesDao.kt
+++ app/src/main/java/com/artemissoftware/demeterrecipes/database/dao/RecipesDao.kt
@@ -1,9 +1,7 @@
 package com.artemissoftware.demeterrecipes.database.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.artemissoftware.demeterrecipes.database.entities.FavoritesEntity
 import com.artemissoftware.demeterrecipes.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -16,4 +14,18 @@ interface RecipesDao {
     @Query("SELECT * FROM recipes_table ORDER BY id ASC")
     fun readRecipes(): Flow<List<RecipesEntity>>
 
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 }
