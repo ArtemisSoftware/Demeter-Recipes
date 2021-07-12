66d09f4d15f57ece403895dc212b0b9d9bb47353
gustavo <gustavo.isaac@gmail.com>
2021-07-12 18:26:17 +0100

    Design fragment_favorite_recipes Layout

--- app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/favourites/FavouriteRecipesFragment.kt
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/favourites/FavouriteRecipesFragment.kt
@@ -8,15 +8,8 @@ import android.view.ViewGroup
 import com.artemissoftware.demeterrecipes.R
 
 
-class FavouriteRecipesFragment : Fragment() {
+class FavouriteRecipesFragment : Fragment(R.layout.fragment_favourite_recipes) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favourite_recipes, container, false)
-    }
 
 
 }
