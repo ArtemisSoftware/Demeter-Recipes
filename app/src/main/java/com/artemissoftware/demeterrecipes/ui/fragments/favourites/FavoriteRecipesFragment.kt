e1090932743837dfed2e60c61d2862920e5a9be7
gustavo <gustavo.isaac@gmail.com>
2021-07-15 16:34:00 +0100

    Display Favorite Recipes

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/favourites/FavoriteRecipesFragment.kt
@@ -0,0 +1,41 @@
+package com.artemissoftware.demeterrecipes.ui.fragments.favourites
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.artemissoftware.demeterrecipes.R
+import com.artemissoftware.demeterrecipes.databinding.FragmentFavoriteRecipesBinding
+import com.artemissoftware.demeterrecipes.ui.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
+
+    private val favoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
+    private val mainViewModel: MainViewModel by viewModels()
+
+
+    private var _binding: FragmentFavoriteRecipesBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        _binding = FragmentFavoriteRecipesBinding.bind(view)
+        setupRecyclerView(binding.favoriteRecipesRecyclerView)
+
+        mainViewModel.readFavoriteRecipes.observe(viewLifecycleOwner, {
+            favoriteRecipesAdapter.setData(it)
+        })
+
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = favoriteRecipesAdapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+}
