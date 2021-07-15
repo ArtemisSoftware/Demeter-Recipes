353ce5fdd2cbb6665bffdd2fd5b143a732a71fc8
gustavo <gustavo.isaac@gmail.com>
2021-07-15 15:44:07 +0100

    Create FavoriteRecipesAdapter class

---
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/favourites/FavoriteRecipesAdapter.kt
@@ -0,0 +1,56 @@
+package com.artemissoftware.demeterrecipes.ui.fragments.favourites
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.artemissoftware.demeterrecipes.database.entities.FavoritesEntity
+import com.artemissoftware.demeterrecipes.databinding.ItemFavoriteRecipeBinding
+import com.artemissoftware.demeterrecipes.ui.fragments.recipes.adapters.RecipesDiffUtil
+
+class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.FavoriteHolder>() {
+
+    private var favoriteRecipes = emptyList<FavoritesEntity>()
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteHolder {
+        return FavoriteHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: FavoriteHolder, position: Int) {
+        val selectedRecipe = favoriteRecipes[position]
+        holder.bind(selectedRecipe)
+    }
+
+    override fun getItemCount(): Int {
+        return favoriteRecipes.size
+    }
+
+    fun setData(newFavoriteRecipes: List<FavoritesEntity>){
+        val favoriteRecipesDiffUtil = RecipesDiffUtil(favoriteRecipes, newFavoriteRecipes)
+        val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
+        favoriteRecipes = newFavoriteRecipes
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+
+    class FavoriteHolder(private val binding: ItemFavoriteRecipeBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(favoritesEntity: FavoritesEntity) {
+            binding.favoritesEntity = favoritesEntity
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): FavoriteHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemFavoriteRecipeBinding.inflate(layoutInflater, parent, false)
+                return FavoriteHolder(binding)
+            }
+        }
+
+    }
+
+}
