7340044e73d867dafb34f3e09c208e79082db28d
gustavo <gustavo.isaac@gmail.com>
2021-08-19 16:22:31 +0100

    RecyclerView - Item Selection State - Fixed

--- app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/favourites/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/artemissoftware/demeterrecipes/ui/fragments/favourites/FavoriteRecipesAdapter.kt
@@ -36,6 +36,9 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity, priv
         val currentRecipe = favoriteRecipes[position]
         holder.bind(currentRecipe)
 
+
+        saveItemStateOnScroll(currentRecipe, holder)
+
         /**
          * Single Click Listener
          * */
@@ -83,6 +86,16 @@ class FavoriteRecipesAdapter(private val requireActivity: FragmentActivity, priv
     private fun applyStatusBarColor(color: Int) {
         requireActivity.window.statusBarColor = ContextCompat.getColor(requireActivity, color)
     }
+
+
+    private fun saveItemStateOnScroll(currentRecipe: FavoritesEntity, holder: FavoriteHolder){
+        if (selectedRecipes.contains(currentRecipe)) {
+            changeRecipeStyle(holder, R.color.cardBackgroundLightColor, R.color.colorPrimary)
+        } else {
+            changeRecipeStyle(holder, R.color.cardBackgroundColor, R.color.strokeColor)
+        }
+    }
+
 
     private fun applySelection(holder: FavoriteHolder, currentRecipe: FavoritesEntity) {
         if (selectedRecipes.contains(currentRecipe)) {
